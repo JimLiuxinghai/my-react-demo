46e9fd5c9de62d0be76d229c8ef1090786d854d3
jimliuxinghai <jimliuxinghai@163.com>
2016-03-11 16:22:08 +0800

    完成静态部分

--- express-react/public/js/demo.js
+++ express-react/public/js/demo.js
@@ -1,36 +1,116 @@
-/**
- * Created by liujim on 16/3/10.
- */
- var Comment = React.createClass({
-    render : function () {
-        return (
-            <div>
-                <h2>{this.props.name}</h2>
-                <div>{this.props.text}</div>
-            </div>
-        )
-    }
-});
-var CommentForm = React.createClass({
-    getInitialState : function () {
-      return {
-        name : '',
-        text : ''
+
+var CommentBox = React.createClass({
+
+  handleSubmit : function (comment) {
+
+  },
+  getData : function (){
+    var _self = this;
+    $.ajax({
+      url : 'data.json',
+      type : 'GET',
+      async : false,
+      success : function(msg){
+        _self.setState({data : msg})
       }
-    },
-
-    render : function () {
-        return (
-            <div>
-                <input className='name' type='text' value={this.state.name} onChange={this.handleName} />
-                <input className='text' type='text' value={this.state.text} onChange={this.handleText} />
-                <br/>
-              <input type='button' value='发表' />
-            </div>
-        )
+    })
+  },
+  getInitialState : function () {
+    return {
+      data : []
     }
-});
+  },
+  componentDidMount : function () {
+    this.getData();
+  },
+  render : function () {
+    return (
+      <div className='commentBox'>
 
+        <CommentList commetData={this.state.data}/>
+        <CommentForm onCommentSubmit={this.handleSubmit}/>
+      </div>
+    )
+  }
+})
+
+var Comment = React.createClass({
+  render:function(){
+    return (
+      <div className='comment'>
+        <h2 className='commentAuthor'>
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+
+    )
+  }
+})
+//修改CommentList组件，让其载入Comment组件
 var CommentList = React.createClass({
-
+  render:function(){
+    console.log(this.props.commetData)
+    var commentNodes = this.props.commetData.map(function(one){
+      return (
+        <Comment author={one.author} key={one.id}>
+          {one.text}
+        </Comment>
+      )
+    })
+    return (
+      <div className='commentList'>
+        {commentNodes}
+      </div>
+    )
+  }
 });
+
+var CommentForm = React.createClass({
+  getInitialState : function(){
+    return {
+      author : '',
+      text : ''
+    }
+  },
+  handleAuthor : function (e){
+      this.setState({
+        author : e.target.value
+      })
+  },
+  handleText : function (e) {
+    this.setState({
+      text : e.target.value
+    })
+  },
+  submit : function () {
+    //e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  },
+  render : function () {
+    return (
+      <div>
+          <div className='mt10'>
+            姓名：<input type='text' onChange={this.handleAuthor}/>
+          </div>
+          <div className='mt10'>
+            内容：<input type='text' onChange={this.handleText}/>
+          </div>
+          <div className='mt10'>
+            <input type="button" value="发表" onClick={this.submit}/>
+          </div>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('container')
+)
